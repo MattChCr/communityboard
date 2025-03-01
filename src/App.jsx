8eaa0b37bd1be54cf7fa67660b08447971e09487
Matt <mattchavez815@gmail.com>
2025-03-01 00:42:16 -0500

    Complete Formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
+import Tatte from './images/Tatte.jpg'
+import TeaDo from './images/TeaDo.jpg'
+import Dunkin from './images/Dunkin.jpg'
+import Starbucks from './images/Starbucks.jpg'
+import CafeMaiko from './images/CafeMaiko.jpg'
+import KungFuTea from './images/KungFuTea.jpg'
+import GreenHaus from './images/Greenhaus.jpeg'
+import Pressed from './images/Pressed.jpeg'
+import FarmersHorse from './images/FarmersHorse.jpg'
+import GongCha from './images/GongCha.jpg'
+import PhinCoffee from './images/PhinCoffee.jpg'
+import Crossing from './images/Crossing.jpg'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+const App = () => {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <body>
+        <h1> Nearby Cafes!</h1>
+        <div className="cardDeck">
+          <div className="cardRow">
+            <Card image={Tatte} name='Tatte' style='Coffee Shop' link='https://www.kungfutea.com/'/>
+            <Card image={TeaDo} name='Tea-Do' style='Boba Shop'/> 
+            <Card image={Dunkin} name='Dunkin' style='Coffee Shop'/> 
+            <Card image={Starbucks} name='Starbucks' style='Coffee Shop'/> 
+          </div>
+          <div className="cardRow">
+            <Card image={CafeMaiko} name='Cafe Maiko' style='Matcha Shop'/>
+            <Card image={KungFuTea} name='Kung Fu Tea' style='Boba Shop'/> 
+            <Card image={GreenHaus} name='Green Haus' style='Coffee Shop'/>
+            <Card image={Pressed} name='Pressed Cafe' style='Coffee Shop'/>  
+          </div>
+          <div className="cardRow">
+            <Card image={FarmersHorse} name='Farmers Horse' style='Coffee Shop'/>
+            <Card image={GongCha} name='Gong Cha' style='Boba Shop'/> 
+            <Card image={PhinCoffee} name='Phin Coffee' style='Coffee Shop'/> 
+            <Card image={Crossing} name='Cafe Crossing' style='Coffee Shop'/> 
+          </div>
+        </div>
+      </body>
+      
+      
+      
+
+    </div>
+      
   )
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../index.css';
+
+const Card = (props) => {
+    return (
+        <div className= "Card">
+            <img src={props.image}/>
+            <h2> {props.name}</h2>
+            <h3> {props.style}</h3> 
+            <br></br>
+            <div class="Button">
+                 <a src={props.link}> View Sites </a>
+                 </div>
+        
+        </div>
+      
+    )
+}
+
+export default Card;
